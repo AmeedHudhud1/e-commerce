c94e26ba69401b3f4758b32be54c9764327951e3
AmeedHudhud <ameedhudhud0@gmail.com>
2024-10-11 23:48:06 +0300

    add pages to app,jsx page

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,90 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './Pages/Home'
-import Root from './Route/Root'
-import NotFound from './Pages/NotFound'
-import Login from './Pages/Login/Login'
-import Register from './Pages/Register/Register'
-import { ToastContainer} from 'react-toastify';
+import React, { useContext, useEffect, useState } from 'react';
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
+import Home from './Pages/User/Home';
+import Root from './Route/Root';
+import NotFound from './Pages/NotFound';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-const router = createBrowserRouter([
-  {
-    element:<Root/>,
-    children:[
-      {
-        path:'/',
-        element:<Home/>
-      },
-      {
-        path:'/login',
-        element:<Login/>
-      },
-      {
-        path:'/register',
-        element:<Register/>
-      },
-      {
-        path:'*',
-        element:<NotFound/>
-      },
-    ]
-  }
-])
-export default function App() {
+import AdminRoot from './Route/AdminRoot';
+import Products from './Pages/Admin/Products/Products';
+import Orders from './Pages/Admin/Orders';
+import Profile from './Pages/Profile';
+import RoleToggleButton from './Components/RoleToggleButton ';
+import UserContextProvider, { UserContext } from './context/User';
+import AddProduct from './Pages/Admin/Products/addProduct';
+import UpdateProduct from './Pages/Admin/Products/updateProduct';
+import DeleteProduct from './Pages/Admin/Products/deleteProduct';
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      element:<Root />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/login',
+          element: <Login />,
+        },
+        {
+          path: '/register',
+          element: <Register />,
+        },
+        {
+          path: '/profile',
+          element: <Profile />,
+        },
+        {
+          path: '*',
+          element: <NotFound />,
+        },
+      ],
+    },
+    {
+      element: <AdminRoot />,
+      children: [
+        {
+          path: '/admin/products',
+          element: <Products />,
+        },
+        {
+          path: '/admin/orders',
+          element: <Orders />,
+        },
+        {
+          path: '/admin/profile',
+          element: <Profile />,
+        },
+        {
+          path: '/admin/product/add',
+          element: <AddProduct/>
+        },
+        {
+          path: '/admin/product/update',
+          element: <UpdateProduct/>
+        },
+        {
+          path: '/admin/product/delete',
+          element: <DeleteProduct/>
+        },
+       
+      ],
+    },
+  ]);
+
   return (
     <>
-    <RouterProvider router={router}/>
-    <ToastContainer />
+    <UserContextProvider>
+      <RouterProvider router={router} />
+    </UserContextProvider>
+      
+      
+      <ToastContainer />
     </>
-  )
-}
+  );
+};
+
+export default App;
