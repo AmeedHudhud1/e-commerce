a25e043d453a0feb1d5d53ece98da1d4524e0b22
AmeedHudhud <ameedhudhud0@gmail.com>
2024-10-11 23:49:48 +0300

    create Home page for user

---
+++ src/Pages/User/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Loader from '../../Components/loader/loader'
+
+export default function Home() {
+  return (
+    <div >
+      <h1>Home</h1>
+     <Loader/>
+    </div>
+  )
+}
