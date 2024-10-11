48d85208c3c8e728f525d4b3c82778554ead2c11
AmeedHudhud <ameedhudhud0@gmail.com>
2024-10-11 23:38:31 +0300

    add Role in navbar for admin

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../context/User";
 
 export default function Navbar() {
+
+  const { role } = useContext(UserContext);
+  const changeRole = (newRole, redirectPath) => {
+    localStorage.setItem('role', newRole); 
+    setRole(newRole); 
+    navigate(redirectPath);
+  };
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-secondary bg-gradient">
-        <div className="container d-flex h-auto ">
+        <div className="container-fluid">
           <div>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
@@ -18,6 +26,48 @@ export default function Navbar() {
                   Register
                 </Link>
               </li>
+              <li className="nav-item">
+                <Link className="nav-link active ms-3 text-white fw-bold" aria-current="page" to="/profile">
+                  Profile
+                </Link>
+              </li>
+
+              {/* Conditionally render the Role dropdown if the role is Admin */}
+              {role === 'Admin' ? (
+                <li className="nav-item dropdown hover-dropdown ms-4">
+                <a
+                  className="nav-link dropdown-toggle text-white fw-bold"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Role
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <Link
+                      className="dropdown-item"
+                      onClick={() => changeRole('admin', '/admin/products')} // Change to admin role and navigate
+                      to="/admin/products" // Optional: Add this to ensure navigation
+                    >
+                      Admin
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="dropdown-item"
+                      onClick={() => changeRole('user', '/')} // Change to user role and navigate
+                      to="/" // Optional: Add this to ensure navigation
+                    >
+                      User
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              ):null}
+
             </ul>
           </div>
         </div>
