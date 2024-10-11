ac59012998659db30f52e8baf8f060868762f59e
AmeedHudhud <ameedhudhud0@gmail.com>
2024-10-11 23:48:44 +0300

    create spinner loader

---
+++ src/Components/loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Loader() {
+  return (
+
+    
+    
+    <div class="spinner-border text-dark" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>
+  );
+}
