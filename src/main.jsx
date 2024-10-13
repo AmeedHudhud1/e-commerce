381b3d9a22891494e0a31e8a6d4f9c3077d49ab4
AmeedHudhud <ameedhudhud0@gmail.com>
2024-10-14 00:13:39 +0300

    try solve problem

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import UserContextProvider from './context/User.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <UserContextProvider>
+
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </UserContextProvider>
 )
