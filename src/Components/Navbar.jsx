c8d5e00feea5838a602597ae3f88bb3ec7b56281
AmeedHudhud <ameedhudhud0@gmail.com>
2024-10-28 20:46:05 +0200

    edit navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,78 +1,182 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../context/User";
 
 export default function Navbar() {
-  const { role, setRole } = useContext(UserContext);
+  const { role, role2, setRole2,setRole } = useContext(UserContext);
   const navigate = useNavigate();
 
   const changeRole = (newRole, redirectPath) => {
-    localStorage.setItem('role', newRole);
-    setRole(newRole); 
+    setRole2(newRole); 
     navigate(redirectPath);
   };
-
   return (
-    <>
-      <nav className="navbar navbar-expand-lg bg-secondary bg-gradient">
-        <div className="container-fluid">
-          <div>
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link active ms-3 text-white fw-bold" aria-current="page" to="/login">
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active ms-3 text-white fw-bold" aria-current="page" to="/register">
-                  Register
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active ms-3 text-white fw-bold" aria-current="page" to="/profile">
-                  Profile
-                </Link>
+    <nav className="navbar navbar-expand-lg bg-secondary bg-gradient">
+      <div className="container-fluid">
+        <div>
+          {
+            
+            role == "" ?
+            (
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active ms-3 text-white fw-bold" onClick={()=>{
+                setRole2("")
+                setRole("")
+              }} to="/login">
+                Login
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active ms-3 text-white fw-bold" to="/register">
+                Register
+              </Link>
+            </li>
+          </ul>
+            ): role == "User" ?
+            (
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+            <Link className="nav-link active ms-3 text-white fw-bold" to="/products">
+                Products
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active ms-3 text-white fw-bold" onClick={()=>{
+                localStorage.setItem("role","")
+                localStorage.setItem("token","")  
+                setRole("")
+                setRole2("")
+              }} to="/login">
+                Logout
+              </Link>
+            </li>
+          </ul>
+            ):(
+              
+              role2 == 'Admin' ?  (
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active ms-3 text-white fw-bold" to="/products">
+                Products
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active ms-3 text-white fw-bold" to="/categorys">
+                Categories
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active ms-3 text-white fw-bold" to="/profile">
+                Orders
+              </Link>
+            </li>
+
+            
+              <li className="nav-item dropdown hover-dropdown ms-4">
+                <a
+                  className="nav-link dropdown-toggle text-white fw-bold"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Role
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <Link
+                      className="dropdown-item"
+                      to="/products"
+                      onClick={() => setRole2('Admin')}
+                    >
+                      Admin
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="dropdown-item"
+                      to="/"
+                      onClick={() => setRole2('User')}
+                    >
+                      User
+                    </Link>
+                  </li>
+                </ul>
               </li>
 
-              {/* Conditionally render the Role dropdown if the role is Admin */}
-              {role === 'Admin' && (
-                <li className="nav-item dropdown hover-dropdown ms-4">
-                  <a
-                    className="nav-link dropdown-toggle text-white fw-bold"
-                    href="#"
-                    id="navbarDropdown"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Role
-                  </a>
-                  <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li>
-                      <Link
-                        className="dropdown-item"
-                        onClick={() => changeRole('admin', '/admin/products')} // Change to admin role and navigate
-                        to="/admin/products" // Optional: Add this to ensure navigation
-                      >
-                        Admin
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        className="dropdown-item"
-                        onClick={() => changeRole('user', '/')} // Change to user role and navigate
-                        to="/" // Optional: Add this to ensure navigation
-                      >
-                        User
-                      </Link>
-                    </li>
-                  </ul>
-                </li>
-              )}
-            </ul>
-          </div>
+              <li className="nav-item">
+              <Link className="nav-link active ms-3 text-white fw-bold" onClick={()=>{
+                localStorage.setItem("role","")
+                localStorage.setItem("token","")
+                setRole("")
+                setRole2("")
+              }} to="/login">
+                Logout
+              </Link>
+            </li>
+            
+          </ul>
+              ):(
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+            <Link className="nav-link active ms-3 text-white fw-bold" to="/products">
+                Products
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active ms-3 text-white fw-bold" onClick={()=>{
+                localStorage.setItem("role","")
+                localStorage.setItem("token","")  
+                setRole("")
+                setRole2("")
+              }} to="/login">
+                Logout
+              </Link>
+            </li>
+
+            <li className="nav-item dropdown hover-dropdown ms-4">
+                <a
+                  className="nav-link dropdown-toggle text-white fw-bold"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Role
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <Link
+                      className="dropdown-item"
+                      to="/products"
+                      onClick={() => setRole2('Admin')}
+                    >
+                      Admin
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="dropdown-item"
+                      to="/"
+                      onClick={() => setRole2('User')}
+                    >
+                      User
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+          </ul>
+              )
+
+              
+            )
+            }
+
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 }
